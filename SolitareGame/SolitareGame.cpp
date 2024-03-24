7d8dbdbafaf64230312c3db51b2e060c24c61fa8
Maryam <mokhammed2007@gmail.com>
2024-03-24 13:22:04 +0300

    1

---
+++ SolitareGame/SolitareGame.cpp
@@ -0,0 +1,12 @@
+﻿// SolitareGame.cpp: определяет точку входа для приложения.
+//
+
+#include "SolitareGame.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
