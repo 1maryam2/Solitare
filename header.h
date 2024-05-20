6b67e1e781860a673c131ed2f6cff264ae641248
1maryam2 <150718466+1maryam2@users.noreply.github.com>
2024-05-20 13:17:50 +0300

    Update header.h

--- header.h
+++ header.h
@@ -1,8 +1,33 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <iostream>
+
 class Box;
+class Game_Field;
+class vis_Card;
+class vis_field;
 
+void Drawing();
+vis_field View(vis_field& field,  Game_Field& f);
+vis_field view_scrol(vis_field& field, Game_Field& f);
 
+class vis_Card{
+    public:
+        sf::Sprite sprite;
+        sf::Texture text;
+        vis_Card &operator =(const vis_Card& v);
+        vis_Card(const vis_Card& v);
+        vis_Card();
+};
+
+class vis_field{
+    public:
+        std::vector<vis_Card> card1[7];
+        std::vector<vis_Card> card2[4];
+        std::vector<vis_Card> card3[2];
+        vis_field &operator =(const vis_field &v);
+        vis_field();
+};
 
 class Card{
     private:
@@ -12,25 +37,45 @@ class Card{
             card_suit_diamonds,//буби
             card_suit_hearts//червы
         };
-        Card& operator =(Card& r);
-        bool operator ==(Card& r);
-        Card();
-        Card(Card& card);
-        card_suit suit;
+        card_suit suit;//масть карты
         int value;//значение карты
         bool visible;//флаг который определяет видно карту или нет
-        friend class Box;
+    public:
+        Card& operator =(const Card& r);
+        bool operator ==(const Card& r);
+        Card();
+        Card(const Card& card);
+        Card(int v, bool vis, card_suit su);
+        int dist_suit(card_suit s);
+    friend class Box;
+    friend class Game_Field;
+    friend vis_field View(vis_field& field,  Game_Field& f);
+    friend vis_field view_scrol(vis_field& field, Game_Field& f);
 };
 
 class Box{
     private:
         std::vector<Card> cards;
     public:
+        Box();
         bool isEmpty();
         void show_last_card();
+    friend class Game_Field;
+    friend vis_field View(vis_field& field,  Game_Field& f);
+    friend vis_field view_scrol(vis_field& field, Game_Field& f);
 };
 
-class Game_Filed{
-    Box main[4];
-    
+class Game_Field{
+    public:
+        Box main[4];
+        Box sim[7];
+        Box base[2];
+        void scrol_base();
+        void plant(Card* card);
+        Card* random_change();
+        void fill_main(int x, int y);
+        void shiftCard(int i,int  x, int y);
+        bool isRightFillmain(Card& card, int x);
+        bool isRightShiftCard(Card& card, int x);
 };
+
